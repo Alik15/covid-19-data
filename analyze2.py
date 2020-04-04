4a0b60e7a9dce6e0656db8760ca65d0b2e24aea4
Albert Gerovitch <albertg@mit.edu>
2020-04-03 20:12:54 -0400

    Added change feature
    - See the change in case numbers (number of new cases), i.e. the first derivative
    - Other minor bug fixes and improvements

--- analyze2.py
+++ analyze2.py
@@ -4,7 +4,6 @@ import matplotlib.pyplot as plt
 import matplotlib.dates as mdates
 import numpy as np
 import requests
-import statistics
 
 
 class DateUtil:
@@ -26,11 +25,6 @@ class DateUtil:
 		locator = mdates.WeekdayLocator(byweekday = mdates.MO)
 		ax.xaxis.set_major_locator(locator)
 
-	def get_day_before(date, days = 1):
-		date_obj = DateUtil.parse_date(date)
-		day_before_obj = date_obj - datetime.timedelta(days = days)
-		return str(day_before_obj.date())
-
 class Region:
 	def __init__(self, name, fips, population):
 		# geographical data
@@ -42,23 +36,25 @@ class Region:
 
 		# COVID-19 data
 		self.cases = {}
-		self.latest_cases_date = None
+		self.latest_cases = 0
 		self.deaths = {}
-		self.latest_deaths_date = None
+		self.latest_deaths = 0
+		self.latest_date = None
 
 	def __eq__(self, other):
 		return self.name == other.name
 
 	def __str__(self):
-		if not self.latest_cases_date or not self.latest_deaths_date:
+		if not self.latest_date:
 			return '{} ({}, pop {})'.format(self.name,
 				self.fips,
 				self.population)
-		return '{} ({}, pop {}): {} Cases, {} Deaths'.format(self.name,
+		return '{} ({}, pop {}): {}, {} ({})'.format(self.name,
 			self.fips,
 			self.population,
-			self.get_latest_cases(False, False),
-			self.get_latest_deaths(False, False))
+			self.latest_cases,
+			self.latest_deaths,
+			self.latest_date.date())
 
 	def __repr__(self):
 		return self.__str__()
@@ -87,140 +83,122 @@ class Region:
 	def update_cases(self, date, number):
 		self.cases[date] = int(number)
 		date_obj = DateUtil.parse_date(date)
-		if not self.latest_cases_date or date_obj > self.latest_cases_date:
-			self.latest_cases_date = date_obj
+		self.latest_cases = max(self.latest_cases, int(number))
+		if not self.latest_date or date_obj > self.latest_date:
+			self.latest_date = date_obj
 
-	def get_cases(self, date = None, per_capita = True, change = False):
+	def get_cases(self, date = None, per_capita = True):
 		if date and date not in self.cases:
 			return None
 		if date:
-			cases = self.cases[date]
-			if change:
-				days_before = [self.cases[DateUtil.get_day_before(date, i)] - self.cases[DateUtil.get_day_before(date, i + 1)] if DateUtil.get_day_before(date, i + 1) in self.cases else np.nan for i in range(5)]
-				cases = np.nanmean(days_before)
-				if np.isnan(cases):
-					return None
-			if per_capita:
-				return cases / self.population * per_capita
-			return cases
+			return self.cases[date] / self.population * per_capita if per_capita else self.cases[date]
 		return self.cases
 
-	def get_latest_cases(self, per_capita = True, change = False):
-		return self.get_cases(str(self.latest_cases_date.date()), per_capita, change) if self.latest_cases_date else None
+	def get_latest_cases(self, per_capita = True):
+		return self.latest_cases / self.population * per_capita if per_capita else self.latest_cases
 
 	############ DEATHS ############
 
 	def update_deaths(self, date, number):
 		self.deaths[date] = int(number)
 		date_obj = DateUtil.parse_date(date)
-		if not self.latest_deaths_date or date_obj > self.latest_deaths_date:
-			self.latest_deaths_date = date_obj
+		self.latest_deaths = max(self.latest_deaths, int(number))
+		if not self.latest_date or date_obj > self.latest_date:
+			self.latest_date = date_obj
 
-	def get_deaths(self, date = None, per_capita = True, change = False):
+	def get_deaths(self, date = None, per_capita = True):
 		if date and date not in self.deaths:
 			return None
 		if date:
-			deaths = self.deaths[date]
-			if change:
-				days_before = [self.deaths[DateUtil.get_day_before(date, i)] - self.deaths[DateUtil.get_day_before(date, i + 1)] if DateUtil.get_day_before(date, i + 1) in self.deaths else np.nan for i in range(5)]
-				deaths = np.nanmean(days_before)
-				if np.isnan(deaths):
-					return None
-			if per_capita:
-				return deaths / self.population * per_capita
-			return deaths
+			return self.deaths[date] * per_capita / self.population if per_capita else self.deaths[date]
 		return self.deaths
 
-	def get_latest_deaths(self, per_capita = True, change = False):
-		return self.get_deaths(str(self.latest_deaths_date.date()), per_capita, change) if self.latest_deaths_date else None
+	def get_latest_deaths(self, per_capita = True):
+		return self.latest_deaths * per_capita / self.population if per_capita else self.latest_deaths
 
 	############ PLOT CASES ############
 
-	def plot_cases(self, start_date, end_date = None, legend = None, labels = True, per_capita = True, change = False):
+	def plot_cases(self, start_date, end_date = None, legend = None, labels = True, per_capita = True):
 		if not end_date:
-			if not self.latest_cases_date:
+			if not self.latest_date:
 				return
-			end_date = str(self.latest_cases_date.date())
+			end_date = str(self.latest_date.date())
 		dates = DateUtil.get_date_range(start_date, end_date)
 		DateUtil.set_date_axes()
 
-		cases = [self.get_cases(str(date), per_capita = per_capita, change = change) for date in dates]
+		cases = [self.get_cases(str(date), per_capita = per_capita) for date in dates]
 		plt.plot(dates, cases, label = legend if legend else self.name)
 		if labels:
 			regex = " {:.2f}" if per_capita else " {:.0f}"
 			plt.text(dates[-1], cases[-1], regex.format(cases[-1]), verticalalignment = 'center')
 
-	def plot_sub_regions_cases(self, start_date, end_date = None, sec = None, labels = True, per_capita = True, change = False):
-		most_cases = list(filter(lambda r: r.get_latest_cases(per_capita, change), self.sub_regions.values()))
-		most_cases.sort(key = lambda r: r.get_latest_cases(per_capita, change), reverse = True)
+	def plot_sub_regions_cases(self, start_date, end_date = None, sec = None, labels = True, per_capita = True):
+		most_cases = list(self.sub_regions.values())
+		most_cases.sort(key = lambda r: r.get_latest_cases(per_capita), reverse = True)
 		if not sec:
 			sec = slice(0, min(20, len(most_cases)))
 
 		for sub_region in most_cases[sec]:
-			sub_region.plot_cases(start_date, end_date, labels = labels, per_capita = per_capita, change = change)
+			sub_region.plot_cases(start_date, end_date, labels = labels, per_capita = per_capita)
 
-		self.plot_setup(per_capita, change, "Cases")
+		self.plot_setup(per_capita, "Cases")
 
 	############ PLOT DEATHS ############
 
-	def plot_deaths(self, start_date, end_date = None, legend = None, labels = True, per_capita = True, change = False):
+	def plot_deaths(self, start_date, end_date = None, legend = None, labels = True, per_capita = True):
 		if not end_date:
-			if not self.latest_deaths_date:
+			if not self.latest_date:
 				return
-			end_date = str(self.latest_deaths_date.date())
+			end_date = str(self.latest_date.date())
 		dates = DateUtil.get_date_range(start_date, end_date)
 		DateUtil.set_date_axes()
 
-		deaths = [self.get_deaths(str(date), per_capita = per_capita, change = change) for date in dates]
+		deaths = [self.get_deaths(str(date), per_capita = per_capita) for date in dates]
 		plt.plot(dates, deaths, label = legend if legend else self.name)
 		if labels:
 			regex = " {:.2f}" if per_capita else " {:.0f}"
 			plt.text(dates[-1], deaths[-1], regex.format(deaths[-1]), verticalalignment = 'center')
 
-	def plot_sub_regions_deaths(self, start_date, end_date = None, sec = None, labels = True, per_capita = True, change = False):
-		most_deaths = list(filter(lambda r: r.get_latest_deaths(per_capita, change), self.sub_regions.values()))
-		most_deaths.sort(key = lambda r: r.get_latest_deaths(per_capita, change), reverse = True)
+	def plot_sub_regions_deaths(self, start_date, end_date = None, sec = None, labels = True, per_capita = True):
+		most_deaths = list(self.sub_regions.values())
+		most_deaths.sort(key = lambda r: r.get_latest_deaths(per_capita), reverse = True)
 		if not sec:
 			sec = slice(0, min(20, len(most_deaths)))
 
 		for sub_region in most_deaths[sec]:
-			sub_region.plot_deaths(start_date, end_date, labels = labels, per_capita = per_capita, change = change)
+			sub_region.plot_deaths(start_date, end_date, labels = labels, per_capita = per_capita)
 
-		self.plot_setup(per_capita, change, "Deaths")
+		self.plot_setup(per_capita, "Deaths")
 
-	def plot_cases_vs_deaths(self, start_date, end_date = None, labels = True, per_capita = True, change = False):
+	def plot_cases_vs_deaths(self, start_date, end_date = None, labels = True, per_capita = True):
 		if not end_date:
-			if not self.latest_cases_date and not self.latest_deaths_date:
+			if not self.latest_date:
 				return
-			end_date_cases = str(self.latest_cases_date.date())
-			end_date_deaths = str(self.latest_deaths_date.date())
-		if end_date_cases:
-			self.plot_cases(start_date, end_date_cases, legend = 'Cases', labels = labels, per_capita = per_capita, change = change)
-		if end_date_deaths:
-			self.plot_deaths(start_date, end_date_deaths, legend = 'Deaths', labels = labels, per_capita = per_capita, change = change)
+			end_date = str(self.latest_date.date())
+		self.plot_cases(start_date, end_date, legend = 'Cases', labels = labels, per_capita = per_capita)
+		self.plot_deaths(start_date, end_date, legend = 'Deaths', labels = labels, per_capita = per_capita)
 
-		self.plot_setup(per_capita, change)
+		self.plot_setup(per_capita)
 
-	def plot_setup(self, per_capita, change, category = None):
+	def plot_setup(self, per_capita, category = None):
 		ax = plt.gca()
 		ax.spines['right'].set_visible(False)
 		ax.spines['top'].set_visible(False)
 
 		plt.xlabel("Date")
-		ylabel = ("Cumulative " if not change else "") + "Number" + (" of " + category if category else "") + (" (Per " + '{:,}'.format(per_capita) + ")" if per_capita else "")
+		ylabel = "Cumulative Number" + (" of " + category if category else "") + (" (Per " + '{:,}'.format(per_capita) + ")" if per_capita else "")
 		plt.ylabel(ylabel)
-		title = ("New " if change else "") + "COVID-19 " + (category if category else "Cases and Deaths") + (" Per " + '{:,}'.format(per_capita) if per_capita else "") + " in " + self.name
+		title = "COVID-19 " + (category if category else "Cases and Deaths") + (" Per " + '{:,}'.format(per_capita) if per_capita else "") + " in " + self.name
 		plt.suptitle(title, y = 0.985, fontsize = 14)
 		subtitle = "Total: "
-		latest_cases, latest_deaths = self.get_latest_cases(False, False), self.get_latest_deaths(False, False)
 		if not category:
-			subtitle += '{:,}'.format(latest_cases) + " Cases, " + '{:,}'.format(latest_deaths) + " Deaths"
+			subtitle += '{:,}'.format(self.latest_cases) + " Cases, " + '{:,}'.format(self.latest_deaths) + " Deaths"
 		if category == "Cases":
-			subtitle += '{:,}'.format(latest_cases)
+			subtitle += '{:,}'.format(self.latest_cases)
 		if category == "Deaths":
-			subtitle += '{:,}'.format(latest_deaths)
+			subtitle += '{:,}'.format(self.latest_deaths)
 		plt.title(subtitle, fontsize = 10)
-		plt.legend(loc = 'upper left')
+		plt.legend()
 		plt.tight_layout(pad = 1)
 		plt.show()
 
@@ -280,7 +258,7 @@ def parse_us_states(filename, US):
 	for row in reader:
 		date, state, fips, cases, deaths = row
 		US.update_cases(date, int(cases) if US.get_cases(date) == None 
-			else (US.get_cases(date, per_capita = False, change = False) + int(cases)))
+			else (US.get_cases(date, per_capita = False) + int(cases)))
 		US.update_deaths(date, int(deaths) if US.get_deaths(date) == None 
 			else US.get_deaths(date, per_capita = False) + int(deaths))
 		state = US.return_sub_region(fips)
@@ -312,18 +290,17 @@ def parse_us_counties(filename, US):
 US, states = setup_regions()
 parse_us_states('us-states.csv', US)
 parse_us_counties('us-counties.csv', US)
+
 # plot US data
 US.plot_cases_vs_deaths('2020-03-01', per_capita = 10000)
 US.plot_sub_regions_cases('2020-03-01', per_capita = 10000)
 US.plot_sub_regions_cases('2020-03-01', sec = slice(2, 22), per_capita = 10000)
 US.plot_sub_regions_deaths('2020-03-01', sec = slice(1, 21), per_capita = False)
-US.plot_sub_regions_cases('2020-03-23', sec = slice(2, 22), per_capita = False, change = True)
 
 # plot state data
 states["Massachusetts"].plot_sub_regions_cases('2020-03-01', per_capita = 10000)
-states["Massachusetts"].plot_sub_regions_cases('2020-03-01', per_capita = False, change = True)
 states["Massachusetts"].plot_sub_regions_deaths('2020-03-16', per_capita = False)
-states["California"].plot_sub_regions_cases('2020-03-01', sec = slice(2, 22), per_capita = 10000)
+states["California"].plot_sub_regions_cases('2020-03-01', per_capita = 10000)
 states["Illinois"].plot_sub_regions_cases('2020-03-01', per_capita = 10000)
 states["Utah"].plot_sub_regions_cases('2020-03-01', per_capita = 10000)
 states["New York"].plot_sub_regions_cases('2020-03-01', per_capita = 10000)
